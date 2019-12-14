4498fadb87aa9db6cbf5f504639ea5acae44d5b9
mtarka <marta.tarka@student.put.poznan.pl>
2019-12-14 14:21:02 +0100

    pr fixes

--- src/main/kotlin/bd/clinic/modules/restTemplate/LabConfig.kt
+++ src/main/kotlin/bd/clinic/modules/restTemplate/LabConfig.kt
@@ -2,19 +2,16 @@ package bd.clinic.modules.restTemplate
 
 import bd.clinic.modules.examinationInfo.ExaminationType.*
 
-class LabConfig {
+object LabConfig {
 
-    companion object {
+    val laboratoryServerInfoMap = mapOf(1 to ServerInfo(8081, "127.0.0.1"))
 
-        val laboratoryServerInfoMap = mapOf(1 to ServerInfo(8081, "127.0.0.1"))
-
-        val examinationLaboratoryMap = mapOf(
-                ROENTGEN to 1,
-                BLOOD_HORMONE_TEST to 1,
-                BLOOD_GLUCOSE_TEST to 1,
-                HEMOGLOBIN_TEST to 1,
-                BLOOD_THYROID_TEST to 1)
-    }
+    val examinationLaboratoryMap = mapOf(
+            ROENTGEN to 1,
+            BLOOD_HORMONE_TEST to 1,
+            BLOOD_GLUCOSE_TEST to 1,
+            HEMOGLOBIN_TEST to 1,
+            BLOOD_THYROID_TEST to 1)
 
     data class ServerInfo(val port: Int, val ipAddr: String)
 }
